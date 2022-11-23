29fc6febb99ecd973cfb8ff1b736250e888a234a
Filip Stenström <hello@world.tld>
2022-11-23 17:41:45 +0100

    Added new quote

--- src/assets/Quotes.js
+++ src/assets/Quotes.js
@@ -3,7 +3,12 @@ const Quotes = [
         id:1,
         quote: "Jag tänker rätt, när jag gör fel",
         date: "2022-7-??"
-    }
+    },
+    {
+        id:2,
+        quote: "Du ska fan inte tro att du är fri, dessutom ska du betala för det också",
+        date: "2021-?-??"
+    },
 ]
 
 export default Quotes;
